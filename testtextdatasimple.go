d71d58d88766c955219a63022c88ab49788105da
martin9w <martin9w@users.noreply.github.com>
2021-11-14 16:00:28 +0100

    Add files via upload
    module and testing

---
+++ testtextdatasimple.go
@@ -0,0 +1,208 @@
+package testtextdatasimple
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	NullString = ""
+	OutVal     = "out value"
+	EstVal     = "est value"
+)
+
+type (
+	TestFuncData struct {
+		Name  string   `json:"name"`
+		Alias string   `json:"alias"`
+		Keys  []string `json:"keys"`
+		Level string   `json:"level"`
+		Datas TestDatas
+	}
+
+	TestData struct {
+		Name   string
+		Level  string            `json:"level"`
+		Params KeyValues         `json:"params"`
+		In     KeyValue          `json:"in"`
+		Out    map[string]string `json:"out"`
+		Est    map[string]string `json:"est"`
+	}
+	KeyValue struct {
+		Key   string `json:"key"`
+		Value string `json:"value"`
+	}
+
+	TestFuncDatas []TestFuncData
+	TestDatas     []TestData
+	KeyValues     []KeyValue
+)
+
+var (
+	jsonString = true
+)
+
+func jsonOut(jsonStr []byte, err error) string {
+	if err != nil {
+		log.Println("testdata: JSON parse error: ", err)
+		return ""
+	}
+	return string(jsonStr)
+}
+
+func (a TestFuncData) String() string {
+	if jsonString {
+		return jsonOut(json.Marshal(a))
+	}
+	return fmt.Sprintf("Name: %s, Alias: %v, Keys: %v, Level: %v, Datas: %v",
+		a.Level, a.Alias, a.Keys, a.Level, a.Datas)
+}
+
+func (a TestData) String() string {
+	if jsonString {
+		return jsonOut(json.Marshal(a))
+	}
+	return fmt.Sprintf("Level: %s, Params: %v, In: %v, Out: %v, Est: %v",
+		a.Level, a.Params, a.In, a.Out, a.Est)
+}
+
+func (a KeyValue) String() string {
+	if jsonString {
+		return jsonOut(json.Marshal(a))
+	}
+	return fmt.Sprintf("Key: %s, Value: %v",
+		a.Key, a.Value)
+}
+
+func init() {
+}
+
+func SetJsonString(onOff bool) {
+	jsonString = onOff
+}
+
+func GetJsonString() bool {
+	return jsonString
+}
+
+func GetTestDataKonfig(filename string) ([]TestFuncData, error) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	config := []TestFuncData{}
+	err = json.Unmarshal([]byte(file), &config)
+	if err != nil {
+		return nil, err
+	}
+	for iFunc := range config {
+		for iData := range config[iFunc].Datas {
+			if config[iFunc].Datas[iData].Name == NullString {
+				config[iFunc].Datas[iData].Name = config[iFunc].Name
+			}
+		}
+	}
+	log.Debug(config)
+	return config, err
+}
+
+func TestDataChecker(keys []string, testData TestData) ([]string, bool) {
+	check := true
+	errors := []string{}
+	for _, k := range keys {
+		outVal, outOk := testData.Out[k]
+		estVal, estOk := testData.Est[k]
+		if outOk && estOk {
+			if outVal != estVal {
+				check = false
+				errors = append(errors,
+					fmt.Sprintf("%s: %s: %s != %s: %s",
+						k, OutVal, testData.Out[k], EstVal, testData.Est[k]))
+			} else {
+				log.Debug(fmt.Sprintf("%s: %s: %s == %s: %s",
+					k, OutVal, testData.Out[k], EstVal, testData.Est[k]))
+			}
+		} else {
+			check = false
+			errorMsg := "Errors in config of testData: "
+			errorMsg = ""
+			if !outOk {
+				errorMsg += fmt.Sprintf("%s: missing %s", k, OutVal)
+			}
+			if !estOk {
+				errorMsg += fmt.Sprintf("%s: missing %s", k, EstVal)
+			}
+			errors = append(errors, errorMsg)
+		}
+	}
+	return errors, check
+}
+
+func SetTestDataLevel(testData TestData, levelDefault string) {
+	level := testData.Level
+	if level == "" {
+		level = levelDefault
+	}
+	switch level {
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	default:
+	}
+}
+
+func GetTestDatasEntry(testdatas []TestFuncData, name string) (*TestFuncData, error) {
+	for _, data := range testdatas {
+		if data.Name == name {
+			return &data, nil
+		}
+	}
+	return nil, errors.New(fmt.Sprintf("no testdatas found for: %s", name))
+}
+
+func GetTestDatasEntryAsAlias(testdatas []TestFuncData, name string) (*TestFuncData, error) {
+	for _, data := range testdatas {
+		if data.Alias != NullString {
+			name = data.Alias
+		}
+		if data.Name == name {
+			return &data, nil
+		}
+	}
+	return nil, errors.New(fmt.Sprintf("no testdatas found for: %s", name))
+}
+
+func GetTestDataParam(testData TestData, name string) string {
+	for _, param := range testData.Params {
+		if param.Key == name {
+			return param.Value
+		}
+	}
+	return NullString
+}
+
+func GetTestDataIn(testData TestData) string {
+	inType := testData.In.Key
+	in := NullString
+	switch inType {
+	case "file":
+		filename := testData.In.Value
+		b, _ := ioutil.ReadFile(filename)
+		in = string(b)
+	case "string":
+		in = testData.In.Value
+	default:
+		in = NullString
+	}
+	return in
+}
---
+++ testtextdatasimple_test.go
@@ -0,0 +1,103 @@
+package testtextdatasimple
+
+import (
+	"fmt"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	TestJsonFile = "testdata/test.json"
+)
+
+var (
+	testFilename  = "testdata/test.txt"
+	testFuncDatas = TestFuncDatas{}
+	testID        = NullString
+)
+
+func init() {
+	datas, err := GetTestDataKonfig(TestJsonFile)
+	if err != nil {
+		log.Fatal(datas, err)
+	}
+	testFuncDatas = datas
+}
+
+func TestFuncId(t *testing.T) {
+	setJson := GetJsonString()
+	testSwitch(t, "TestFuncId")
+	if log.IsLevelEnabled(log.DebugLevel) {
+		SetJsonString(true)
+		fmt.Println(testFuncDatas)
+		SetJsonString(setJson)
+	}
+}
+
+func TestFuncAlias(t *testing.T) {
+	setJson := GetJsonString()
+	testFuncData, error := GetTestDatasEntry(testFuncDatas, "TestFuncAlias")
+	if error != nil || testFuncData.Alias == NullString {
+		log.Error("NoAlias")
+	}
+	testSwitch(t, testFuncData.Alias)
+	if log.IsLevelEnabled(log.DebugLevel) {
+		SetJsonString(true)
+		fmt.Println(testFuncDatas)
+		SetJsonString(setJson)
+	}
+}
+
+func TestFuncNoAlias(t *testing.T) {
+	testFuncData, error := GetTestDatasEntry(testFuncDatas, "TestFuncNoAlias")
+	if error != nil {
+		log.Error("Error getting Alias")
+	}
+	testSwitch(t, "TestFuncNoAlias")
+	if testFuncData.Alias != NullString {
+		log.Error("No Alias but found one")
+	}
+	log.Debug("OK - No Alias was set")
+}
+
+func testChecker(t *testing.T, keys []string, testData TestData, found bool, name string) {
+	if !found {
+		t.Error("nothing found for: ", name)
+	}
+	errors, check := TestDataChecker(keys, testData)
+	if !check {
+		t.Errorf("%v", errors)
+	}
+
+}
+func testSwitch(t *testing.T, name string) {
+	testFuncData, error := GetTestDatasEntry(testFuncDatas, name)
+	if error != nil {
+		t.Error(error)
+		return
+	}
+	if len(testFuncData.Datas) == 0 {
+		SetTestDataLevel(TestData{}, testFuncData.Level)
+	} else {
+		for _, testData := range testFuncData.Datas {
+			SetTestDataLevel(testData, testFuncData.Level)
+			foundData := false
+			switch name {
+			case "TestFuncAlias", "TestFuncId":
+				idParam := GetTestDataParam(testData, "id")
+				id := idParam
+				found := id != NullString
+				if found {
+					testData.Out = map[string]string{
+						"id": id,
+						"in": "InValue",
+					}
+				}
+				foundData = found
+			case "TestFuncNoAlias":
+			}
+			testChecker(t, testFuncData.Keys, testData, foundData, name)
+		}
+	}
+}
